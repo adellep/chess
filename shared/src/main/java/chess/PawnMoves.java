b058f88f6cbb0b216f6d1708966e08d1e97dfd89
adellep <adellep@byu.edu>
2024-09-17 11:35:17 -0600

    8 pawn tests pass (5 to go)

--- shared/src/main/java/chess/PawnMoves.java
+++ shared/src/main/java/chess/PawnMoves.java
@@ -10,15 +10,17 @@ public class PawnMoves {
 
         var piece = board.getPiece(myPosition);
         if (piece.getPieceType() == ChessPiece.PieceType.PAWN) {
-            if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+            if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) { //positives
                 getPawnMove(moves, myPosition, myPosition, +1, 0, board);
-                if (isFirstTurnWhite(myPosition, board)) {
+                if (isFirstTurnWhite(myPosition, board) && !isBlocked(myPosition, +1, board)
+                        && !isBlocked(myPosition, +2, board)) {
                     getPawnMove(moves, myPosition, myPosition, +2, 0, board); //for 1st turn
                 }
             }
-            if (piece.getTeamColor() == ChessGame.TeamColor.BLACK) {
+            if (piece.getTeamColor() == ChessGame.TeamColor.BLACK) { //negatives
                 getPawnMove(moves, myPosition, myPosition, -1, 0, board);
-                if (isFirstTurnBlack(myPosition, board)) {
+                if (isFirstTurnBlack(myPosition, board) && !isBlocked(myPosition, -1, board)
+                        && !isBlocked(myPosition, -2, board)) {
                     getPawnMove(moves, myPosition, myPosition, -2, 0, board);
                 }
             }
@@ -35,7 +37,7 @@ public class PawnMoves {
         return row >= 1 && row <= 8 && col >= 1 && col <= 8;
     }
 
-    boolean isEnemy(ChessPosition origPos, ChessPosition newPos, ChessBoard board) {
+    boolean isEnemy(ChessPosition origPos, ChessPosition newPos, ChessBoard board) { //change to check diagonally forward squares
         ChessPiece pieceOrigPos = board.getPiece(origPos);
         ChessPiece pieceNewPos = board.getPiece(newPos);
 
@@ -50,9 +52,7 @@ public class PawnMoves {
         ChessPiece pieceOrigPos = board.getPiece(pos);
 
         if (pieceOrigPos.getPieceType() == ChessPiece.PieceType.PAWN) {
-            if (pos.getRow() == 2 && pieceOrigPos.getTeamColor() == ChessGame.TeamColor.WHITE) {
-                return true;
-            }
+            return pos.getRow() == 2;
         }
         return false;
     }
@@ -61,11 +61,16 @@ public class PawnMoves {
         ChessPiece pieceOrigPos = board.getPiece(pos);
 
         if (pieceOrigPos.getPieceType() == ChessPiece.PieceType.PAWN) {
-            if (pos.getRow() == 7 && pieceOrigPos.getTeamColor() == ChessGame.TeamColor.BLACK) {
-                return true;
-            }
+            return pos.getRow() == 7;
         }
         return false;
+    }
+
+    boolean isBlocked(ChessPosition pos, int rowDir, ChessBoard board) {
+        var oneSquareFor = new ChessPosition(pos.getRow() + rowDir, pos.getColumn());
+        var twoSquaresFor = new ChessPosition(oneSquareFor.getRow() + rowDir, oneSquareFor.getColumn());
+
+        return board.getPiece(oneSquareFor) != null || board.getPiece(twoSquaresFor) != null;
     }
 
     void getPawnMove(Collection<ChessMove> moves, ChessPosition origPos,
@@ -75,12 +80,9 @@ public class PawnMoves {
         if (!isRealSquare(newPos)) {
             return;
         }
-//        if(isFirstTurn(origPos, board)) {
-//
-//        }
-//        if (isEnemy(origPos, newPos, board)) {
-//            moves.add(new ChessMove(origPos, newPos, null)); //will have to change null
-//        }
+        if (board.getPiece(newPos) != null) {
+            return;
+        }
         else if (board.getPiece(newPos) == null) {
             moves.add(new ChessMove(origPos, newPos, null)); //will have to change null
         }
