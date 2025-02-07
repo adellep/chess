0a08234f1a59205fee1582333d4e75ce9dc69a15
adellep <adellep@byu.edu>
2025-02-07 10:32:42 -0700

    starting makeMove

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -89,7 +89,12 @@ public class ChessGame {
      * @throws InvalidMoveException if move is invalid
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
-        throw new RuntimeException("Not implemented");
+        ChessPosition startPosition = move.getStartPosition();
+        ChessPiece piece = board.getPiece(startPosition);
+
+        if (piece == null) {
+            throw new RuntimeException("Not implemented");
+        }
     }
 
     /**
