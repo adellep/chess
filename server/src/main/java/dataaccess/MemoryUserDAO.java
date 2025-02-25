6a6a6898d5a903f3b4f6a28cc233671c014856ac
adellep <adellep@byu.edu>
2025-02-25 11:53:16 -0700

    added data access classes

---
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -0,0 +1,5 @@
+package dataaccess;
+
+public class MemoryUserDAO implements UserDAO {
+    //actually implement functions declared in interfaces
+}
---
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -0,0 +1,6 @@
+package dataaccess;
+
+public interface UserDAO {
+    //data access class - store and retrieve server data
+    //create, read, update, delete
+}
