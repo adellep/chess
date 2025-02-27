24a4cb583550db157bd29b26768dc124956e4360
adellep <adellep@byu.edu>
2025-02-26 17:45:52 -0700

    register success test

--- server/src/test/java/service/DataAccessTests.java
+++ server/src/test/java/service/DataAccessTests.java
@@ -1,10 +1,9 @@
 package service;
 
-import dataaccess.MemoryAuthDAO;
-import dataaccess.MemoryGameDAO;
-import dataaccess.MemoryUserDAO;
+import dataaccess.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import request.RegisterRequest;
 
 public class DataAccessTests {
 
@@ -17,5 +16,19 @@ public class DataAccessTests {
         var result = clearService.clear();
 
         Assertions.assertNull(result.message());
+    }
+
+    @Test
+    public void registerSuccess() throws DataAccessException, ResponseException {
+        var userDAO = new MemoryUserDAO();
+        var authDAO = new MemoryAuthDAO();
+        var registerService = new RegisterService(userDAO, authDAO);
+        var request = new RegisterRequest("a", "p", "a@gmail.com");
+        var response = registerService.register(request);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.authToken());
+        Assertions.assertNotNull(userDAO.getUser("a"));
+        Assertions.assertEquals("a", response.username());
     }
 }
